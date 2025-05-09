11aaa5e8b8220ac36444bddcfea995c610436f89
Ben Meddeb <bmeddeb@asu.edu>
2025-05-09 13:02:55 -0700

    FIX-Warning in asyncio.rs

--- python/src/asyncio.rs
+++ python/src/asyncio.rs
@@ -1,6 +1,6 @@
 use pyo3::prelude::*;
 
-pub fn init_module(_py: Python<'_>, m: &Bound<'_, PyModule>) -> PyResult<()> {
+pub fn init_module(_py: Python<'_>, _m: &Bound<'_, PyModule>) -> PyResult<()> {
     // We'll implement async functionality later
     Ok(())
 }
